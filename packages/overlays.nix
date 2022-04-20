93da68c715e14855c4620b580bdd59cc435c9a9c
kuwii <kuwii.someone@gmail.com>
2022-04-20 20:08:51 +0800

    [programs] remove microsoft-edge
    Remove Microsoft Edge because it is already available in Nixpkgs.

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -1,7 +1,3 @@
 [(final: prev: {
-  nixt = {
-    microsoft-edge = prev.callPackage (import ./microsoft-edge).stable {};
-    microsoft-edge-beta = prev.callPackage (import ./microsoft-edge).beta {};
-    microsoft-edge-dev = prev.callPackage (import ./microsoft-edge).dev {};
-  };
+  nixt = { };
 })]
