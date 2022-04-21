a71e112154f830f125f0e021b6691d4cfe313ef1
kuwii <kuwii.someone@gmail.com>
2022-04-21 18:37:32 +0800

    [programs] add compression software

---
+++ modules/programs/compression/default.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+let
+  allowUnfree = config.nixpkgs.config.allowUnfree;
+  cfg = config.nixt.programs.compression;
+  inherit (lib) mkIf mkMerge mkOption types;
+in
+{
+  options.nixt.programs.compression = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install common compression software.";
+    };
+  };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [
+        unzip libsForQt5.ark p7zip
+      ];
+    })
+    (mkIf (allowUnfree && cfg.enable) {
+      environment.systemPackages = with pkgs; [
+        unrar
+      ];
+    })
+  ];
+}
