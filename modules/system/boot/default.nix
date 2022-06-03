ec0caefb3a2eb996a979b2c2178d71ffb5596f04
kuwii <kuwii.someone@gmail.com>
2022-06-03 22:55:58 +0800

    [system] boot: add bootscreen support

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -16,14 +16,24 @@ in
       default = "/boot/efi";
       description = "Location of the mounted EFI partition.";
     };
+    bootscreen.enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable bootscreen based on Plymouth.";
+    };
   };
 
-  config = (mkIf cfg.enable {
-    boot.loader.grub.enable = true;
-    boot.loader.grub.version = 2;
-    boot.loader.grub.efiSupport = true;
-    boot.loader.grub.device = "nodev";
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.efi.efiSysMountPoint = cfg.mountPoint; 
-  });
+  config = mkMerge [
+    (mkIf cfg.enable {
+      boot.loader.grub.enable = true;
+      boot.loader.grub.version = 2;
+      boot.loader.grub.efiSupport = true;
+      boot.loader.grub.device = "nodev";
+      boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.efi.efiSysMountPoint = cfg.mountPoint;
+    })
+    (mkIf (cfg.enable && cfg.bootscreen.enable) {
+      boot.plymouth.enable = true;
+    })
+  ];
 }
