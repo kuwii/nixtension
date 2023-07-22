094ddfb13e6569763c9891373921faaa892f7248
kuwii <kuwii.someone@gmail.com>
2023-07-22 10:23:48 +0800

    [programs] neovim: add shortcuts of diagnotic

--- modules/programs/neovim/features/diagnostic/default.nix
+++ modules/programs/neovim/features/diagnostic/default.nix
@@ -3,7 +3,15 @@
 let
   cfg = config.nixtension.programs.neovim.diagnostic;
   keymap = cfg.keymap;
-  withConfig = builtins.replaceStrings [ ] [ ];
+  withConfig = builtins.replaceStrings ([
+    "__TOGGLE__" "__REFRESH__"
+    "__OPEN_DOCUMENT__" "__OPEN_WORKSPACE__" "__OPEN_QUICKFIX__" "__OPEN_LOCLIST__"
+    "__OPEN_REFERENCES__" "__OPEN_DEFINITIONS__" "__OPEN_TYPE_DEFINITIONS__"
+  ]) (with keymap; [
+    toggle refresh
+    open-document open-workspace open-quickfix open-loclist
+    open-references open-definitions open-type-references
+  ]);
 in
 lib.mkMerge [
   (import ../build-feature.nix {
