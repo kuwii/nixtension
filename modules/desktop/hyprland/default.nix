98e6e2ca05ae3f47b8d8619dcb67cdd967aa1b7d
kuwii <kuwii.someone@gmail.com>
2023-07-27 08:23:54 +0800

    [desktop] hyprland: add system tools

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -11,24 +11,34 @@ in
 
   config = mkMerge [
     (mkIf cfg.enable {
+      networking.networkmanager.enable = true;
+      hardware.bluetooth.enable = true;
+
       services.xserver.enable = true;
       services.pipewire.enable = true;
       security.polkit.enable = true;
 
       programs.hyprland.enable = true;
       programs.hyprland.xwayland.enable = true;
+      programs.xwayland.enable = true;
       programs.dconf.enable = true;
 
       environment.sessionVariables = {
         NIXOS_OZONE_WL = "1";
       };
       environment.systemPackages = with pkgs; [
-        kitty                               # terminal
-        dunst                               # notification daemon
-        libsForQt5.polkit-kde-agent         # authentication agent
-        nixtension.polkit-kde-agent-runner  #
-        libsForQt5.qt5.qtwayland            # qt wayland support
-        qt6.qtwayland                       #
+        # terminal
+        kitty
+        # notification daemon
+        dunst libnotify
+        # authentication agent
+        libsForQt5.polkit-kde-agent nixtension.polkit-kde-agent-runner
+        # qt wayland support
+        libsForQt5.qt5.qtwayland qt6.qtwayland
+        # network manager
+        networkmanagerapplet
+        # x compatibility
+        xorg.xhost
       ];
     })
     (mkIf cfg.sddm.enable {
