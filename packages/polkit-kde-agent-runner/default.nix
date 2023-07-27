ca6b6ec271ed0f21df99e6695abf91ef917d2408
kuwii <kuwii.someone@gmail.com>
2023-07-27 22:32:39 +0800

    [packages] fix polkit-kde-agent-runner

--- packages/polkit-kde-agent-runner/default.nix
+++ packages/polkit-kde-agent-runner/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name="polkit-kde-agent-runner-0.0.1";
   unpackPhase = "true";
   installPhase = ''
-    BIN=$out/bin/run-polkit-kde-agent-runner
+    BIN=$out/bin/run-polkit-kde-agent
     mkdir -p $out/bin
     echo "#!/usr/bin/env bash" > $BIN
     echo "${libsForQt5.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1" >> $BIN
