8f4b823cb77c32f853014710f682f03d2f9fbacf
kuwii <kuwii.someone@gmail.com>
2023-08-12 16:44:50 +0800

    [programs] neovim: update example

--- modules/programs/neovim/example/neovim.nix
+++ modules/programs/neovim/example/neovim.nix
@@ -9,15 +9,18 @@
   nixtension.programs.neovim.basic-settings.enable = true;
 
   # Status bar feature
+  # Based on vim-airline & vim-airline-clock
   nixtension.programs.neovim.status-bar.enable = true;
 
   # File explorer feature
+  # Based on nvim-tree.lua & nvim-web-devicons
   nixtension.programs.neovim.explorer.enable = true;
   nixtension.programs.neovim.explorer.keymap = {
     toggle = "<space>e";
   };
 
   # Git feature
+  # Based on gitsigns.nvim
   nixtension.programs.neovim.git.enable = true;
   nixtension.programs.neovim.git.keymap = {
     toggle = "<space>gg";
@@ -34,6 +37,7 @@
   };
 
   # Fuzzy finder feature
+  # Based on telescope.nvim & telescope-zf-native.nvim & plenary.nvim
   nixtension.programs.neovim.fuzzy-finder.enable = true;
   nixtension.programs.neovim.fuzzy-finder.keymap = {
     files = "<space>fg";
@@ -43,6 +47,7 @@
   };
 
   # Terminal feature
+  # Based on toggleterm.nvim
   nixtension.programs.neovim.terminal.enable = true;
   nixtension.programs.neovim.terminal.direction = "horizontal";
   nixtension.programs.neovim.terminal.keymap = {
@@ -51,24 +56,31 @@
   };
 
   # Current word feature
+  # Based on vim_current_word
   nixtension.programs.neovim.current-word.enable = true;
   nixtension.programs.neovim.current-word.keymap = {
     toggle = "<space>w";
   };
 
   # Markdown feature
+  # Based on markdown-preview.nvim & vim-markdown
   nixtension.programs.neovim.markdown.enable = true;
   nixtension.programs.neovim.markdown.keymap = {
     toggle = "<space>p";
   };
 
   # LSP feature
+  # Based on nvim-lspconfig
   nixtension.programs.neovim.nvim-lspconfig.enable = true;
+  # Based on lua-language-server & LuaJIT
   nixtension.programs.neovim.lua-support.enable = true;
+  # Based on rnix-lsp
   nixtension.programs.neovim.nix-support.enable = true;
+  # Based on bash-language-server
   nixtension.programs.neovim.bash-support.enable = true;
 
   # Diagnostic feature
+  # Based on Trouble & LSP Colors & nvim-web-devicons
   nixtension.programs.neovim.diagnostic.enable = true;
   nixtension.programs.neovim.diagnostic.keymap = {
     toggle = "<space>de";
