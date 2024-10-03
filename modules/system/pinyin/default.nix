04e6ec3b01b667591616750ab19ebb71b5ccf72d
kuwii <kuwii.someone@gmail.com>
2024-10-03 11:36:19 +0800

    [system] pinyin: fix for new configuration structure

--- modules/system/pinyin/default.nix
+++ modules/system/pinyin/default.nix
@@ -14,7 +14,8 @@ in
   };
 
   config = (mkIf cfg.enable {
-    i18n.inputMethod.enabled = "fcitx5";
+    i18n.inputMethod.enable = true;
+    i18n.inputMethod.type = "fcitx5";
     i18n.inputMethod.fcitx5.addons = with pkgs; [ fcitx5-gtk fcitx5-chinese-addons ];
   });
 }
