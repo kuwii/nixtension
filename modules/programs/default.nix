1e8f8ab7d3fb44ea71457b97bef318ae97f57dd6
kuwii <kuwii.someone@gmail.com>
2024-10-05 16:09:55 +0800

    [programs] add options to enable all

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,5 +1,9 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
+let
+  cfg = config.nixtension.programs;
+  inherit (lib) mkIf mkMerge mkOption types;
+in
 {
   imports = [
     ./compression-software
@@ -9,5 +13,37 @@
     ./neovim
     ./system-tools
     ./toys
+  ];
+
+  options.nixtension.programs = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install supported programs.";
+    };
+
+    enable-cli = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install supported command line programs.";
+    };
+  };
+
+  config = mkMerge [
+    (mkIf (cfg.enable || cfg.enable-cli) {
+      nixtension.programs = {
+        compression-software.enable = true;
+        git.enable = true;
+        hardware-tools.enable = true;
+        neovim.enable = true;
+        system-tools.enable = true;
+        toys.enable = true;
+      };
+    })
+    (mkIf cfg.enable {
+      nixtension.programs = {
+        joplin.enable = true;
+      };
+    })
   ];
 }
