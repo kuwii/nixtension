4242197f1945af5541b0ba6bfa2cd55ebe6c1bac
kuwii <kuwii.someone@gmail.com>
2025-09-21 16:29:22 +0800

    [system] sound: add wireplumber

--- modules/system/sound/default.nix
+++ modules/system/sound/default.nix
@@ -28,10 +28,12 @@ in
       services.pulseaudio.enable = false;
       services.pipewire = {
         enable = true;
+        audio.enable = true;
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
+        wireplumber.enable = true;
       };
     })
     (mkIf (cfg.enable && cfg.pulseaudio.enable) {
